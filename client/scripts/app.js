6375c0a7d5adc1c88d8707f3be07c1613669d700
AdamCmiel <adam_cmiel@yahoo.com>
2014-01-20 16:36:51 -0800

    can send messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,85 +1,72 @@
-var renderMessageTemplate = "<p class='room'>Roomname: <%= roomname %></p><p class='user'>User: <%= username %></p><p class='message'> <%= text %></p><p class='time'>Time: <%= createdAt %>";
+var url = 'https://api.parse.com/1/classes/chatterbox';
+var username = window.location.search.split('=')[1];
 
-var url = 'https://api.parse.com/1/classes/chatterbox'; 
-
-var renderChat = function(result){
-  $('<li></li>').html(_.template(renderMessageTemplate, result)).appendTo('.chats');  
-}
-
-
-var entityMap = {
-    "&": "&amp;",
-    "<": "&lt;",
-    ">": "&gt;",
-    '"': '&quot;',
-    "'": '&#39;',
-    "/": '&#x2F;',
-    "'": '"'
+var renderChat = function(data) {
+    var $li = $('<li></li>');
+    var $username = $('<p class="name"></p>').text('User: @' + data.username);
+    var $chatroom = $('<p></p>').text('Room: -' + data.roomname);
+    var $date = $('<p></p>').text('Date: ' + data.createdAt);
+    var $message = $('<p></p>').text(data.text);
+    $li.append($username)
+        .append($chatroom)
+        .append($date)
+        .append($message);
+    $('.chats').append($li);
 };
 
-var escapeHTML = function(htmlString) {
-  return String(htmlString).replace(/[&<>"'\/'"]/g, function (s) {
-    return entityMap[s];
-  });
+var parseGet = function(data) {
+    var results = data.results;
+    _.each(results, function(result) {
+        renderChat(result);
+    });
 };
 
-var parseUndefines = function(object){
-  _.each(object, function(val, key){
-    if (val===undefined){
-      object[key] = 'N/A';
-    }
-  });
-  return object;
+var postMessage = function(message) {
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: message,
+        contentType: 'application/json',
+        success: function(data) {
+            console.log('success', data);
+        },
+        error: function(data) {
+            console.log('error', data);
+        }
+    });
 };
 
-var parseGet = function(data){
-  var results = data.results;
-  _.each(results, function(result){
-    renderChat(parseUndefines(result));
-  });
-};
-
-var postMessage = function(message){
-  $.ajax({
-    url: url,
-    type: 'POST',
-    data: message,
-    contentType: 'application/json',
-    success: function(data){
-      console.log('success', data);
-    },
-    error: function(data){
-      console.log('error', data);
-    }
-  });
-};
-
-var getMessages = function(){
-  $.getJSON(url, {order: '-createdAt'}, parseGet);
+var getMessages = function(options) {
+    options || (options = {
+        order: '-createdAt',
+        limit: 1000
+    });
+    $.getJSON(url, options, parseGet);
 };
 
 var roomname = 'testING';
 
-var createMessage = function(message){
-  postMessage(
-    JSON.stringify({
-      username: username,
-      text:     message,
-      roomname: roomname
-    })
-  );
+var createMessage = function(message) {
+    postMessage(
+        JSON.stringify({
+            username: username,
+            text: message,
+            roomname: roomname
+        })
+    );
 };
-$('.sendMessage').on('submit', function(e){
-  e.preventDefault();
-  createMessage($('input[name="message"]').val());
-});
 
-$(document).ready(function(){
-  var username = window.location.search.split('=')[1];
-  getMessages();
-  $('.username').text('Hello, @'+username); 
-  $('.getNewMessages').on('click', function(e){
-    e.preventDefault();
+$(document).ready(function() {
+    var username = window.location.search.split('=')[1];
+    var roomname = '4chan';
     getMessages();
-  });
+    $('.username').text('Hello, @' + username);
+    $('.getNewMessages').on('click', function(e) {
+        e.preventDefault();
+        getMessages();
+    });
+    $('form').submit(function(e) {
+        e.preventDefault();
+        createMessage($('input[name="message"]').val());
+    });
 });
